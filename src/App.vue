2c375e3096b140269f3db36bc9d53754be665b6f
ilamuhil <106134789+ilamuhil0907@users.noreply.github.com>
2022-12-04 19:40:31 +0530

    initial commit vue poc

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
